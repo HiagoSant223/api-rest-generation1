2c01a711a448f555f865a2c68dfa0bdc1b0019fa
Hiago Oliveira <hiagosantos223@gmail.com>
2023-10-25 16:40:57 -0300

    Swagger

--- src/main/java/demo/aluno/doc/SwaggerConfig.java
+++ src/main/java/demo/aluno/doc/SwaggerConfig.java
@@ -2,8 +2,10 @@ package demo.aluno.doc;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -11,6 +13,27 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+    private Contact contato() {
+        return new Contact(
+                "Hiago Santos Oliveira",
+                "https://github.com/HiagoSant223",
+                "hiagosantos223@gmail.com");
+    }
+    private ApiInfoBuilder informacoesApi() {
+
+        ApiInfoBuilder apiInfoBuilder = new ApiInfoBuilder();
+
+        apiInfoBuilder.title("API Restful Generation");
+        apiInfoBuilder.description("API exemplo de uso de Springboot REST API desenvolvida com Java");
+        apiInfoBuilder.version("1.0");
+        apiInfoBuilder.termsOfServiceUrl("Termo de uso: Open Source");
+        apiInfoBuilder.license("Licença - Hiago Oliveira");
+        apiInfoBuilder.licenseUrl("https://www.linkedin.com/in/hiago-oliveira-911b00169/");
+        apiInfoBuilder.contact(this.contato());
+
+        return apiInfoBuilder;
+    }
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
